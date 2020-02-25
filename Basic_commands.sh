b8db0971e08e6e80aa0395ffd77da4b99c81131e
root <root@ip-172-31-35-49.ap-south-1.compute.internal>
2020-02-25 05:25:56 +0000

    script reduced

--- Basic_commands.sh
+++ Basic_commands.sh
@@ -1,8 +1,4 @@
-sudo su
-cd /home/ec2-user/jenkins
 mkdir Dir_from_jenkins_job
 cd Dir_from_jenkins_job
 touch file_by_jenkins_job
 touch file_by_jenkins_job_1
-sleep 5
-tar cvzf archive_by_jenkins_job.tar.gz .
